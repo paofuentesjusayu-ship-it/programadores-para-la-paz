725b4f9ac4851eca36bb92037ff63af26aea9adc
Lorena Fuentes <paofuentesjusayu@gmail.com>
2026-06-03 02:22:23 +0000

    Actividad semana 5 completada

--- semana5/server.js
+++ semana5/server.js
@@ -1,26 +1,21 @@
 const express = require('express');
 const app = express();
 
-// Middleware para procesar JSON en las peticiones POST
 app.use(express.json());
 
-// Ruta principal
 app.get('/', (req, res) => {
   res.send('Servidor activo');
 });
 
-// Ruta de saludo
 app.get('/saludo', (req, res) => {
   res.send('Hola comunidad');
 });
 
-// Ruta con parámetro :nombre
 app.get('/mensaje/:nombre', (req, res) => {
   const nombre = req.params.nombre;
   res.send('Hola ' + nombre);
 });
 
-// Ruta POST para recibir reportes
 app.post('/reporte', (req, res) => {
   const mensaje = req.body.mensaje;
   res.json({
@@ -29,7 +24,6 @@ app.post('/reporte', (req, res) => {
   });
 });
 
-// Iniciar servidor
 app.listen(3000, () => {
   console.log('Servidor ejecutándose en puerto 3000');
 });
